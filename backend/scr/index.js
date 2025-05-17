1d33fd6dc0b0ed8f84cfb8731e5dd98bc5f24bd4
NTDarshan <darshannt2034@gmail.com>
2025-05-17 20:57:35 +0530

    completed backend apis

---
+++ backend/scr/index.js
@@ -0,0 +1,19 @@
+import express from "express";
+import authRoutes from "./routes/auth.routes.js";
+import dotenv from "dotenv";
+import { connectDB } from "./lib/db.js";
+import cookieParser from "cookie-parser";
+import messageRoutes from "./routes/message.routes.js";
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT;
+
+app.use(express.json());
+app.use(cookieParser());
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`);
+  connectDB();
+});
